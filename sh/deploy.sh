b0f91d12749c57e41525dc4ac2917916e332a3f7
ddiiwoong <ddiiwoong@gmail.com>
2020-06-18 17:35:42 +0900

    Add CircleCI and Deploy Shell

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,36 +1,28 @@
 #!/bin/sh
 
-# If a command fails then the deploy stops
-set -e
-
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
 fi
-git commit -m "$msg"
 
-# Push source and build repos.
-git push origin master
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
 
-# blog 저장소 Commit & Push
-git add .
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
 
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+echo "Removing existing files"
+rm -rf public/*
 
-git push origin master
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+#echo "Pushing to github"
+#git push --all
