377b053efc26addec64c61327a1968ba181d3908
ddiiwoong <ddiiwoong@gmail.com>
2020-06-19 17:39:21 +0900

    remove posts

--- app.py
+++ app.py
@@ -12,20 +12,5 @@ db = client.exporterhub
 def home():
     return render_template('index.html')
 
-
-@app.route('/api/list', methods=['GET'])
-def exporter_list():
-	return jsonify({'result': 'success', 'msg': 'get 연결되었습니다!'})
-
-@app.route('/api/like', methods=['POST'])
-def exporter_list():
-	return jsonify({'result': 'success', 'msg': 'post 연결되었습니다!'})
-
-
-@app.route('/api/delete', methods=['POST'])
-def exporter_list():
-	return jsonify({'result': 'success', 'msg': 'delete 연결되었습니다!'})
-
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
