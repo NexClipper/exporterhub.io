753b972509567582a2e08b490159d309a0dbbf2f
HHJ-S2 <berdle0122@gmail.com>
2020-12-02 15:50:10 +0900

    Add: detail page upload

--- src/components/ReadmeContent/ReadmeContent.js
+++ src/components/ReadmeContent/ReadmeContent.js
@@ -1,28 +1,42 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import axios from "axios";
 import styled from "styled-components";
+import remarkMarkdown from "./remarkMarkdown";
 import { FiShare2 } from "react-icons/fi";
 import { AiFillStar } from "react-icons/ai";
 
 const ReadmeContent = () => {
+  const { id } = useParams();
+  const [exporterInfo, setExporterInfo] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://10.153.5.73:8000/exporters/${id}`).then(res => {
+      // setReadmeContent(remarkMarkdown(res.data.data));
+      setExporterInfo(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
+  const readmeContent = remarkMarkdown(exporterInfo.readme);
+
   return (
     <>
       <Header>
         <Container>
           <OpenSourceInfo>
-            <HeaderLogo
-              src="./images/prometheus_logo.png"
-              alt="오픈소스 로고"
-            />
+            <HeaderLogo src="/images/prometheus_logo.png" alt="오픈소스 로고" />
             <ListWrap>
               <List>
-                <Name>My SQL Server Exporter</Name>
+                <Name>{exporterInfo.name}</Name>
                 <ShareIcon>
                   <FiShare2 />
                 </ShareIcon>
               </List>
               <List>
-                <Category>Database</Category>
+                <Category>{exporterInfo.category}</Category>
                 <StarIcon>
-                  <AiFillStar /> 12
+                  <AiFillStar /> {exporterInfo.stars}
                 </StarIcon>
               </List>
             </ListWrap>
@@ -32,9 +46,10 @@ const ReadmeContent = () => {
       <Main>
         <Container>
           <ReadmeTitle>README</ReadmeTitle>
-          <Aside>
-            <AddToCart>ADD TO MY CART</AddToCart>
-          </Aside>
+          <MarkdownBody
+            dangerouslySetInnerHTML={{ __html: readmeContent }}
+          ></MarkdownBody>
+          <Aside></Aside>
         </Container>
       </Main>
     </>
@@ -119,9 +134,104 @@ const Main = styled.main`
 `;
 
 const ReadmeTitle = styled.h4`
+  margin-bottom: 45px;
   font-size: 26px;
   font-weight: 500;
   letter-spacing: 0.08rem;
+`;
+
+const MarkdownBody = styled.div`
+  * {
+    line-height: 1.5;
+  }
+
+  code {
+    margin: 0;
+    padding: 0.2em 0.4em;
+    background-color: rgba(27, 31, 35, 0.05);
+    border-radius: 6px;
+    font-size: 13px;
+    font-family: "Noto Sans KR", sans-serif;
+  }
+
+  pre {
+    margin-bottom: 16px;
+    padding: 16px;
+    overflow: auto;
+    line-height: 1.45;
+    background-color: #f6f8fa;
+    border-radius: 6px;
+    font-size: 13px;
+
+    tt,
+    code {
+      background-color: #f6f8fa;
+      line-height: 1.6;
+    }
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 24px 0 16px;
+    line-height: 1.25;
+  }
+
+  h1,
+  h2 {
+    padding-bottom: 0.3em;
+    border-bottom: 1px solid #eaecef;
+    font-weight: 600;
+  }
+
+  h2 {
+    font-size: 1.5em;
+    font-weight: 500;
+  }
+
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-size: 1.25em;
+    font-weight: 400;
+  }
+
+  p {
+    margin-bottom: 16px;
+    font-size: 15px;
+  }
+
+  blockquote {
+    padding: 0 1em;
+    border-left: 0.25em solid #dfe2e5;
+    color: #6a737d;
+  }
+
+  ul {
+    margin-bottom: 16px;
+    padding-left: 2em;
+    list-style-type: disc;
+
+    li {
+      line-height: 2;
+    }
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  hr {
+    height: 0.25em;
+    padding: 0;
+    margin: 24px 0;
+    background-color: #e1e4e8;
+    border: 0;
+  }
 `;
 
 const Aside = styled.aside`
@@ -130,16 +240,6 @@ const Aside = styled.aside`
   top: 50%;
   transform: translateY(-50%);
   width: 300px;
-`;
-
-const AddToCart = styled.button`
-  display: block;
-  width: 100%;
-  padding: 15px;
-  background: #53a28c;
-  border-radius: 3px;
-  text-align: center;
-  color: #fff;
 `;
 
 export default ReadmeContent;
