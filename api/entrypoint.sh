151ffa6c1111ffd1e94349b7812f5d2069005a43
Ralf Yang <goody80-762@gmail.com>
2020-12-09 15:16:55 +0900

    update for DB init with API server runs by urls.py switching

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -50,6 +50,7 @@ case $parm in
 		Check_db
 		python manage.py migrate 
 		cp -fv ./exporterhub/urls_back.py ./exporterhub/urls.py
+		echo "Modify the file: ./exporterhub/urls_back.py -> ./exporterhub/urls.py"
 		python manage.py runserver 0.0.0.0:$port
 		;;
 	scheduler)
