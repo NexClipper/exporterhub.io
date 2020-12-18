00400793b57e80f3c546a567ea4c9672fbe65e20
Ralf Yang <goody80762@gmail.com>
2020-12-18 18:18:54 +0900

    update for public_service checker

--- src/components/Header/HeaderMenu.js
+++ src/components/Header/HeaderMenu.js
@@ -1,49 +1,54 @@
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import PUBLIC_SERVICE from "../../config";
 
 const HEADER_MENUS = [
-  { title: "Company", link: "https://www.nexclipper.io/" },
-  { title: "Blog", link: "https://nexclipper.github.io/blog/" }
+	{ title: "Company", link: "https://www.nexclipper.io/" },
+	{ title: "Blog", link: "https://nexclipper.github.io/blog/" }
 ];
 
 const HeaderMenu = () => {
-  const { push } = useHistory();
-  return (
-    <Div>
-      {HEADER_MENUS.map((menu, idx) => (
-        <a href={menu.link} key={idx}>
-          {menu.title}
-        </a>
-      ))}
-      <span
-        onClick={() => {
-          push("/admin");
-          window.location.reload();
-        }}
-      >
-        Admin
-      </span>
-    </Div>
-  );
+	const { push } = useHistory();
+	return (
+		<Div>
+			{HEADER_MENUS.map((menu, idx) => (
+				<a href={menu.link} key={idx}>
+					{menu.title}
+				</a>
+			))}
+			{
+				(PUBLIC_SERVICE = "n" && (
+					<span
+						onClick={() => {
+							push("/admin");
+							window.location.reload();
+						}}
+					>
+						Admin
+					</span>
+				))
+			}
+		</Div>
+	);
 };
 
 const Div = styled.div`
-  display: flex;
-  align-items: center;
-  font-weight: 700;
-  @media ${({ theme }) => theme.media.mobile} {
-    display: none;
-  }
-  a {
-    margin-left: 30px;
-    color: #000000;
-    cursor: pointer;
-  }
-  span {
-    margin-left: 30px;
-    color: tomato;
-    cursor: pointer;
-  }
+	display: flex;
+	align-items: center;
+	font-weight: 700;
+	@media ${({ theme }) => theme.media.mobile} {
+		display: none;
+	}
+	a {
+		margin-left: 30px;
+		color: #000000;
+		cursor: pointer;
+	}
+	span {
+		margin-left: 30px;
+		color: tomato;
+		cursor: pointer;
+	}
 `;
 
 export default HeaderMenu;
