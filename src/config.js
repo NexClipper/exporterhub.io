a10b605dc9a854fb7f6e2cbdc5c420c5726a1886
Ralf Yang <goody80762@gmail.com>
2020-12-22 01:00:11 +0000

    update for host protocal control of  config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 export const API_SURVER = process.env.REACT_APP_API_URL;
 export const PUBLIC_SERVICE = process.env.REACT_APP_API_STATUS; //- if the PUBLIC_SERVICE is y then "ADMIN" will be remove. or "ADMIN" button will be ok.
-export const EXPORTERS_API = `http://${API_SURVER}:8000`;
-export const EXPORTER_API = `http://${API_SURVER}:8000/exporters`;
-export const CATEGORIES_API = `http://${API_SURVER}:8000/categories`;
-export const EXPORTER_ADMIN_API = `http://${API_SURVER}:8000/exporter`;
-export const TOKEN_API = `http://${API_SURVER}:8000/token`;
+export const EXPORTERS_API = `${API_SURVER}:8000`;
+export const EXPORTER_API = `${API_SURVER}:8000/exporters`;
+export const CATEGORIES_API = `${API_SURVER}:8000/categories`;
+export const EXPORTER_ADMIN_API = `${API_SURVER}:8000/exporter`;
+export const TOKEN_API = `${API_SURVER}:8000/token`;
