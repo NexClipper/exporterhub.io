e78e23705e3ca18cc701303c8d0555573346ba89
banana1019 <jyho930@gmail.com>
2021-03-22 05:40:14 +0000

    Modify: backend initial setting - 환경별로 settings 파일 분리 - exporter, user, headtoken app 생성

--- api/my_settings.py
+++ api/my_settings.py
@@ -1,16 +1,15 @@
 import os
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
+SECRET_KEY = 'supersecretkey'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get('APP_DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('DB_NAME', 'db.sqlite'),    
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', None),
-        'PORT': os.environ.get('DB_PORT', None),
-        'CONN_MAX_AGE': 600,
+    'default' : {
+        'ENGINE'  : 'django.db.backends.mysql',
+        'NAME'    : "exporterhub",
+        'USER'    : "root",
+        'PASSWORD': 'secret',
+        'HOST'    : "127.0.0.1",
+        'PORT'    : "3306",
     }
 }
