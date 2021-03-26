13eec90b88396a9818e6a3745d46b6bfad473133
yangahh <toaur6802@naver.com>
2021-03-26 18:15:31 +0000

    wip

--- api/user/views.py
+++ api/user/views.py
@@ -5,10 +5,11 @@ import requests
 from django.views            import View
 from django.http             import JsonResponse
 from django.db.models        import Q
+from django.core.exceptions  import ObjectDoesNotExist
 
-from exporterhub.settings.local    import SECRET_KEY, ALGORITHM
 from .models                 import User, UserType, Bucket, Star
-
+from exporter.models         import Exporter
+from user.utils              import login_decorator
 
 
 class GithubLoginView(View):
@@ -26,13 +27,12 @@ class GithubLoginView(View):
             usertype_name     = "user" if User.objects.filter().exists() else "admin"
             
 
-            if User.objects.filter(username=user_data.get('login').exists():
-                user = User.objects.get(username=user_data.get('login')
+            if User.objects.filter(username=username.exists():
+                user  = User.objects.get(username=username)
                 token = jwt.encode({'id':user.id, 'usertype':user.type.name}, SECRET_KEY, algorithm=ALGORITHM)
                 
                 return JsonResponse({'message' : 'SUCCESS', 'access_token':token}, status = 200)
             
-
             user = User.objects.create(
                 username          = username,
                 email             = email,
@@ -47,4 +47,25 @@ class GithubLoginView(View):
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
+class ProfileView(View):
+    #@login_decorater
+    def patch(self, request):
+        try:
+            user         = request.user
+            data         = json.load(request.body)
+            email        = data.get('email', user.email)
+            fullname     = data.get('name', user.fullname)
+            organization = data.get('organization', user.organization)
 
+            user = User.object.get(id=user.id)
+            user.email        = email
+            user.fullname     = fullname
+            user.organization = organization
+            user.save()
+
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'NO_USER'}, status=400)
+
+    def delete(self, reuquest):
