d6187434bae6ef4a799c0d643cf34144cc9865a8
93jm <93_jm@naver.com>
2021-03-26 16:26:52 +0900

    ADD : Dashboard, Alert Components

--- src/components/ContentDetail/components/Dashboard.js
+++ src/components/ContentDetail/components/Dashboard.js
@@ -1,7 +1,50 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+import Dataviewer from "./Dataviewer";
 
 const Dashboard = () => {
-  return <div></div>;
+  const [data, setData] = useState();
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const handleMode = () => {
+    setIsEditMode(!isEditMode);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    const url =
+      "https://api.github.com/repos/yoosaemsol/github-test/contents/test.json";
+    axios
+      .get(url)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        alert("fail");
+      });
+  };
+  return (
+    <Container>
+      {data && (
+        <Dataviewer
+          data={data}
+          isEditMode={isEditMode}
+          setIsEditMode={setIsEditMode}
+          handleMode={handleMode}
+        />
+      )}
+    </Container>
+  );
 };
 
 export default Dashboard;
+
+const Container = styled.div`
+  ${({ theme }) => theme.container}
+  position: relative;
+`;
