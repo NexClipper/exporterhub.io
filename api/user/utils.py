5556566ff9b0c4c3c0f0d8b9e90b983da2a91e06
yangahh <toaur6802@naver.com>
2021-03-27 07:23:02 +0000

    Modify: login view conflict 해결

--- api/user/utils.py
+++ api/user/utils.py
@@ -1,17 +1,10 @@
 import jwt
 import json
 
-<<<<<<< HEAD
-from django.http    import JsonResponse
-from my_settings    import SECRET_KEY, ALGORITHM
-
-from user.models    import User
-=======
 from django.http import JsonResponse
 from django.conf import settings
 
 from user.models import User
->>>>>>> feature/profile
 
 def login_check(func):
     def wrapper(self, request, *args, **kwargs):
