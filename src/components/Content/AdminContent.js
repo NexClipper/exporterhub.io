213606a02ca000b015e990bfb6b94da407829a3f
93jm <93_jm@naver.com>
2021-03-29 16:31:10 +0900

    ADD : FE_permisson

---
+++ src/components/Content/AdminContent.js
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+
+const AdminContent = ({ admin }) => {
+  return (
+    <AdminCard>
+      <AdminInfo>
+        <ProfileImg src={admin.profileImageUrl} />
+        <Info>
+          <h4>{admin.username}</h4>
+          <span>{admin.usertype}</span>
+        </Info>
+      </AdminInfo>
+      <AdminResign>Resign</AdminResign>
+    </AdminCard>
+  );
+};
+
+export default AdminContent;
+
+const AdminCard = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  margin-bottom: 40px;
+  padding: 0px 20px 0px 30px;
+  background-color: #ffffff;
+`;
+
+const AdminInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-self: center;
+`;
+
+const ProfileImg = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-right: 15px;
+  border-radius: 50%;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h4 {
+    color: #2767cf;
+    margin-bottom: 5px;
+  }
+
+  span {
+    color: #596068;
+  }
+`;
+
+const AdminResign = styled.button`
+  text-align: center;
+  width: 64px;
+  height: 27px;
+  border-radius: 27px;
+  padding: 5px;
+  color: #c6474e;
+  background-color: #fafbfc;
+  border: 1px solid #d9dbdb;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f1f1f1;
+  }
+`;
--- src/components/MyBucket/components/Permission.js
+++ src/components/MyBucket/components/Permission.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import AdminContent from "../../Content/AdminContent";
+
+//필터변경
+import { filterByUser } from "../../../store/actions/exporterActions";
+import { SearchOutlined } from "@ant-design/icons";
+import axios from "axios";
 
 const Permission = () => {
+  const [adminArr, setAdminArr] = useState([]);
+  const token = sessionStorage.getItem("access_token");
+  const dispatch = useDispatch();
+  const inputHandler = (e) => {
+    // const payload = { filterType: "value", data: e.target.value.toLowerCase() };
+    // dispatch(filterByUser(payload));
+  };
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "http://10.153.5.102:8000/user/admin",
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then((res) => {
+        setAdminArr(res.data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <Container>
-      <ReadmeTitle>Permission</ReadmeTitle>
+      <PermissionHeader>
+        <PermissionTitle>Permission</PermissionTitle>
+        <Div>
+          <SearchOutlined className="search_icon" />
+          <input onChange={inputHandler} type="text" placeholder="Search" />
+        </Div>
+      </PermissionHeader>
+      <AdminContainer>
+        {adminArr.map((admin, index) => {
+          return <AdminContent admin={admin} key={index} />;
+        })}
+      </AdminContainer>
     </Container>
   );
 };
@@ -12,13 +51,60 @@ const Permission = () => {
 export default Permission;
 
 const Container = styled.div`
-  ${({ theme }) => theme.container}
+  display: flex;
+  flex-direction: column;
   position: relative;
+  ${({ theme }) => theme.container}
+
+  .search_icon {
+    position: relative;
+    left: 30px;
+    @media ${({ theme }) => theme.media.mobile} {
+      position: absolute;
+      left: 12px;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+  input {
+    /* @media ${({ theme }) => theme.media.mobile} {
+      width: 100%;
+      margin: 0;
+    } */
+    width: 300px;
+    height: 40px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 4px;
+    padding-left: 35px;
+  }
 `;
 
-const ReadmeTitle = styled.h4`
+const PermissionHeader = styled.div`
+  display: flex;
+  align-items: center;
   margin-bottom: 45px;
+`;
+
+const PermissionTitle = styled.h4`
   font-size: 30px;
   font-weight: 500;
   letter-spacing: 0.08rem;
+`;
+
+const Div = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+  margin-right: auto;
+  /* @media ${({ theme }) => theme.media.mobile} {
+      position: relative;
+      display: block;
+      margin: 15px 0 0;
+    } */
+`;
+
+const AdminContainer = styled.div`
+  ${({ theme }) => theme.container}
+  height: 400px;
+  overflow-y: auto;
 `;
