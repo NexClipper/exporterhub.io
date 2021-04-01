b7c3486a666def916764ec5f682f83ac5f93189e
93jm <93_jm@naver.com>
2021-04-01 17:20:15 +0900

    Fix : solve conflict

--- src/components/ContentDetail/components/Alert.js
+++ src/components/ContentDetail/components/Alert.js
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import Dataviewer from "./Dataviewer";
+import remarkMarkdown from "../remarkMarkdown";
 const Alert = ({ title, githubToken }) => {
   const [githubContent, setGithubContent] = useState();
   const [isEditMode, setIsEditMode] = useState(false);
-  const [sha, setSha] = useState();
+  const [mdSha, setMdSha] = useState();
+  const [fileSha, setFileSha] = useState();
   const handleMode = () => {
     setIsEditMode(!isEditMode);
   };
@@ -16,29 +18,58 @@ const Alert = ({ title, githubToken }) => {
     getData();
   }, [isEditMode]);
   const getData = () => {
-    const url = `https://api.github.com/repos/Exporterhubv3/editor_test/contents/${title}/${title}_alert.yaml`;
-    console.log("여기", url);
+    const mdUrl = `https://api.github.com/repos/Exporterhubv3/editor_test/contents/${title}/${title}_alert.md`;
+    const fileUrl = `https://api.github.com/repos/Exporterhubv3/editor_test/contents/${title}/${title}_alert.yaml`;
     axios
-      .get(url)
+      .get(
+        mdUrl,
+        {},
+        {
+          headers: {
+            Authorization: `token ${githubToken}`,
+          },
+        }
+      )
       .then((res) => {
         setGithubContent(decodeURIComponent(escape(atob(res.data.content))));
-        setSha(res.data.sha);
+        setMdSha(res.data.sha);
         console.log("github에서 코드 가져왔어!");
       })
       .catch((error) => {
         setGithubContent("Null");
       });
+    axios
+      .get(
+        fileUrl,
+        {},
+        {
+          headers: {
+            Authorization: `token ${githubToken}`,
+          },
+        }
+      )
+      .then((res) => {
+        setFileSha(res.data.sha);
+      })
+      .catch((error) => {
+        console.log("file Alert Sha error");
+        console.log(error);
+      });
   };
+  const markDownContent = remarkMarkdown(githubContent);
   return (
     <Container>
       <Dataviewer
         githubContent={githubContent}
+        markDownContent={markDownContent}
         isEditMode={isEditMode}
         handleMode={handleMode}
         title={title}
-        type="_alert.yaml"
+        // type="_alert.yaml"
+        file=".yaml"
+        type="_alert"
         githubToken={githubToken}
-        sha={sha}
+        mdSha={mdSha}
       />
     </Container>
   );
